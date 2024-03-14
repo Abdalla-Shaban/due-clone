5c4dc2ea519f5d00830255056aa7234560526b17
abdalla <abdallashaban584@gmail.com>
2024-03-14 03:18:24 +0200

    ui: update sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,101 +1,116 @@
 "use client";
-import { BsGrid } from "react-icons/bs";
-import { FiLayers } from "react-icons/fi";
-import { PiUserGearLight, PiGraduationCapLight } from "react-icons/pi";
-import { CiMoneyCheck1, CiBookmark, CiFlag1 } from "react-icons/ci";
-import { RiArrowRightSLine, RiArrowLeftSLine } from "react-icons/ri";
 import Link from "next/link";
 import Image from "next/image";
-import logo from "../public/due-icon.svg";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
-export const sidebarLinks = [
-  {
-    title: "dashboard",
-    href: "/",
-    icon: <BsGrid size={18} />,
-  },
-  {
-    title: "projects",
-    href: "/projects",
-    icon: <FiLayers size={18} />,
-  },
+import { AiOutlineHome } from "react-icons/ai";
+import {
+  MdOutlineManageAccounts,
+  MdOutlineInsertDriveFile,
+  MdOutlineFlag,
+  MdOutlineBookmarkBorder,
+  MdOutlineLocalLibrary,
+} from "react-icons/md";
+import logo from "../public/due-icon.svg";
+import Accordion from "./Accordion";
+import RippleEffect from "./RippleEffect";
+
+export const organizationLinks = [
   {
     title: "user management",
     href: "/users",
-    icon: <PiUserGearLight size={18} />,
+    icon: <MdOutlineManageAccounts size={23} />,
   },
   {
     title: "report templates",
     href: "/templates",
-    icon: <CiMoneyCheck1 size={18} />,
-  },
-  {
-    title: "questions bank",
-    href: "/qbank",
-    icon: <CiBookmark size={18} />,
+    icon: <MdOutlineInsertDriveFile size={23} />,
   },
   {
     title: "key issues",
     href: "/key-issues",
-    icon: <CiFlag1 size={18} />,
+    icon: <MdOutlineFlag size={23} />,
   },
   {
-    title: "tutorial",
-    href: "/tutorial",
-    icon: <PiGraduationCapLight size={18} />,
+    title: "questions bank",
+    href: "/qbank",
+    icon: <MdOutlineBookmarkBorder size={23} />,
   },
 ];
 
-const Sidebar = () => {
+const Sidebar = ({ open }: SidebarControlTypes) => {
   const pathname = usePathname();
-  const [hidden, setHidden] = useState(false);
   return (
     <aside
-      className={`group relative bg-[#f8fafc] shadow-md py-6 px-4 duration-300 ${
-        hidden ? "w-[75px]" : "w-[255px]"
+      className={`overflow-hidden relative bg-white duration-300 ${
+        open ? "w-0 opacity-0" : "w-[255px]"
       }`}
     >
-      <button
-        type="button"
-        onClick={() => setHidden(!hidden)}
-        className="absolute w-[25px] h-[25px] right-[-12.5px] z-20 bg-[#fff] shadow-md rounded-full flex items-center justify-center cursor-pointer duration-300 opacity-0 group-hover:opacity-100 hover:bg-[#5378f4] hover:text-[#fff]"
-      >
-        {hidden ? (
-          <RiArrowRightSLine size={18} />
-        ) : (
-          <RiArrowLeftSLine size={18} />
-        )}
-      </button>
-      <div className="mb-5 px-3">
+      <div className="h-[70px] pl-6 flex items-center border-b-slate-200 border-r-slate-200 border-[1px]">
         <Link className="flex items-center gap-1" href="/">
-          <Image src={logo} alt="due logo" width={20} height={20} />
-          <h1
-            className={`text-4xl self-end font-bold text-[#5378f4] ${
-              hidden && "hidden"
-            }`}
-          >
-            Due
-          </h1>
+          <Image src={logo} alt="due logo" width={20} height={20} priority />
+          <h1 className={`text-4xl self-end font-bold text-primary`}>Due</h1>
         </Link>
       </div>
-      <ul className="flex flex-col gap-2">
-        {sidebarLinks.map((link) => {
-          return (
-            <li key={link.title}>
-              <Link
-                className={`flex items-center gap-3 text-sm capitalize p-3 ${
-                  pathname == link.href && "shadow-md bg-[#fff] rounded-md"
-                }`}
-                href={link.href}
+      <div className="flex flex-col">
+        <span className="block px-3 py-4 text-gray-500">Workspace</span>
+        <RippleEffect>
+          <Link
+            className={`flex items-center gap-2 capitalize py-3 px-4 ${
+              pathname == "/"
+                ? "bg-blue-100 text-blue-700 border-r-[3px] border-r-primary"
+                : "hover:bg-slate-200"
+            }`}
+            href="/"
+          >
+            <AiOutlineHome size={23} />
+            <span className={`${pathname == "/" && "font-semibold"}`}>
+              Dashboard
+            </span>
+          </Link>
+        </RippleEffect>
+        <Accordion pathname={pathname} />
+        <hr className="my-2" />
+        <span className="block px-3 py-4 text-gray-500">Organization</span>
+        {organizationLinks.map((link) => (
+          <RippleEffect key={link.href}>
+            <Link
+              className={`flex items-center gap-2 capitalize py-3 px-4 ${
+                pathname.includes(link.href)
+                  ? "bg-blue-50 text-blue-700 border-r-[3px] border-r-primary"
+                  : "hover:bg-slate-200"
+              }`}
+              href={link.href}
+            >
+              {link.icon}
+              <span
+                className={`${pathname.includes(link.href) && "font-semibold"}`}
               >
-                {link.icon}
-                <span className={`${hidden && "hidden"}`}>{link.title}</span>
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
+                {link.title}
+              </span>
+            </Link>
+          </RippleEffect>
+        ))}
+        <hr className="my-2" />
+        <span className="block px-3 py-4 text-gray-500">Help</span>
+        <RippleEffect>
+          <Link
+            className={`flex items-center gap-2 capitalize py-3 px-4 ${
+              pathname.includes("tutorial")
+                ? "bg-blue-50 text-blue-700 border-r-[3px] border-r-primary"
+                : "hover:bg-slate-200"
+            }`}
+            href="/tutorial"
+          >
+            <MdOutlineLocalLibrary size={23} />
+            <span
+              className={`${pathname.includes("tutorial") && "font-semibold"}`}
+            >
+              Tutorial
+            </span>
+          </Link>
+        </RippleEffect>
+        <hr className="my-3" />
+      </div>
     </aside>
   );
 };
